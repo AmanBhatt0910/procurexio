960e02ec442489cc6c805d433f97c6a8c5bb3c54
Aman Bhatt <amanbhatt0910@gmail.com>
2026-03-29 14:45:28 +0530

    updated migrations things

--- src/sql/7_constraints.sql
+++ src/sql/7_constraints.sql
@@ -2,8 +2,16 @@
 -- FINAL CONSTRAINTS (run last)
 -- ============================================================
 
+-- users → vendors
 ALTER TABLE users
 ADD CONSTRAINT fk_users_vendor
 FOREIGN KEY (vendor_id)
 REFERENCES vendors(id)
 ON DELETE SET NULL;
+
+-- invitations → vendors
+ALTER TABLE invitations
+ADD CONSTRAINT fk_invitations_vendor
+FOREIGN KEY (vendor_id)
+REFERENCES vendors(id)
+ON DELETE CASCADE;
